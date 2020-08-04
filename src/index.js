d5d8f9ea4e454980d7d231c720c3471001263a39
ericposas <web_dev@tutanota.com>
2020-08-04 14:06:59 -0500

    added randomized line and dot colors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import * as d3 from 'd3'
 import './style.scss'
 import * as random from 'random'
+import * as colors from 'css-color-names'
 
+// console.log(
+// 	Object.entries(colors.default)
+// )
 
 const doIt = () => {
 
@@ -14,8 +18,11 @@ const doIt = () => {
 	let svg = d3.select('#svg')
 	.attr('width', w).attr('height', h)
 	.style('border', '1px solid black')
-
+	.style('background', 'black')
+	
 	const createData = (n) => (new Array(n).fill(1).map((elt, idx) => ([random.int(1, 100), random.int(1, 100), idx])).sort((a, b) => b[0] - a[0]))
+
+	const getRandColor = () => Object.entries(colors.default)[random.int(0, 148)][0]
 
 	let dataLen = 0
 
@@ -36,14 +43,18 @@ const doIt = () => {
 	const drawPath = data => {
 		if (svg.selectAll('path')) svg.selectAll('path').remove()
 
+		let lineClr = getRandColor()
+
 		let path = svg.selectAll('path')
-		.data([data], d => d)
+		.data([data], d => d) // we wrap data in brackets here to include the entire
+		// data set for a single element; otherwise, we will end up appending
+		// n number of paths, which we don't want
 		.enter().append('path')
 
 		path
 		.attr('d', lineMkr)
 		.attr('fill', 'none')
-		.attr('stroke', 'black')
+		.attr('stroke', lineClr)
 
 		if (path && path.node()) {
 			let totalLength = path.node().getTotalLength()
@@ -59,6 +70,7 @@ const doIt = () => {
 
 	const start = () => {
 		let data = createData(random.int(10, 100))
+		let dotClr = getRandColor()
 
 		let circles = circle_group
 		.selectAll('circle')
@@ -72,7 +84,7 @@ const doIt = () => {
 			.attr('cx', d => scaleX(d[0]))
 			.attr('cy', d => scaleY(d[1]))
 			.attr('r', 3)
-			.attr('fill', 'red')
+			.attr('fill', dotClr)
 		} else {
 			circles.exit()
 			.remove()
@@ -81,7 +93,7 @@ const doIt = () => {
 			.attr('cx', d => scaleX(d[0]))
 			.attr('cy', d => scaleY(d[1]))
 			.attr('r', 3)
-			.attr('fill', 'red')
+			.attr('fill', dotClr)
 		}
 
 		dataLen = data.length
